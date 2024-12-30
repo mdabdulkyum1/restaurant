79c36f43bc2b7b5d1d4ee54417fcdfa11452f3c7
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-12-30 09:06:06 +0600

    banner done

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './Banner.css'
+import slide1 from "../../../assets/home/01.jpg";
+import slide2 from "../../../assets/home/02.jpg";
+import slide3 from "../../../assets/home/03.png";
+
+
+const Banner = () => {
+
+  useEffect(() => {
+    const slideWrapper = document.querySelector(".carousel");
+    slideWrapper.style.height = "650px";
+  }, []);
+
+
+  return (
+    <Carousel
+      autoPlay={true}
+      interval={3000}  
+      infiniteLoop={true} 
+      showThumbs={true} 
+      showArrows={true}  
+      showStatus={false} 
+      dynamicHeight={true} 
+      stopOnHover={true}
+      emulateTouch={true}  
+      useKeyboardArrows={true} 
+    >
+      <div>
+        <img src={slide1} alt="Delicious food" />
+      </div>
+      <div>
+        <img src={slide2} alt="Tasty dishes" />
+      </div>
+      <div>
+        <img src={slide3} alt="Yummy meals" />
+      </div>
+    </Carousel>
+  );
+};
+
+export default Banner;
