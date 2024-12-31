979ccb62aed27af6b4981a70222fba3c122eb029
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-12-31 09:11:10 +0600

    chef servece sections done

---
+++ src/pages/Home/ChefService/ChefService.jsx
@@ -0,0 +1,21 @@
+
+const ChefService = () => {
+    return (
+        <div
+        className="hero min-h-[60vh] bg-[url('assets/home/chef-service.jpg')] bg-fixed bg-cover bg-no-repeat">
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-4xl rounded-lg py-16 bg-white text-black">
+            <h1 className="mb-5 text-5xl font-bold">Bistro Boss</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default ChefService;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import Banner from "./Banner/Banner";
 import Featured from "./Category/Category";
-;
+import ChefService from "./ChefService/ChefService";
+
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
       <Featured></Featured>
+      <ChefService></ChefService>
+      
     </>
   );
 };
