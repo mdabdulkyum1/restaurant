4aa5caa1344e3ce17c00c3144b16abd1b21aa8cc
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-12-31 14:56:07 +0600

    footer done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar/Navbar"
+import Footer from "../components/Footer/Footer"
 
 
 function Layout() {
@@ -7,6 +8,7 @@ function Layout() {
     <div className="container mx-auto">
         <Navbar></Navbar>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
